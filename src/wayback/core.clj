4a0ef45f5eb666e09975b28ff39419679c418f72
Wing Yung <wing@yelp.com>
2013-10-13 09:44:51 -0700

    it's ugly, but this does what i want

--- src/wayback/core.clj
+++ src/wayback/core.clj
@@ -1,11 +1,18 @@
 (ns wayback.core
   (:use [clj-xpath.core :only [$x $x:tag $x:text $x:attrs $x:attrs* $x:node]])
   (:import java.io.File)
+  (:import java.util.Date)
+  (:import java.util.GregorianCalendar)
   )
+
+(def ms-diff
+  (-
+   (.getTime (.getTime (GregorianCalendar. 2013 7 18)))
+   (.getTime (.getTime (GregorianCalendar. 2010 6 28)))))
 
 (defn get-post-filenames
   [dir]
-  (filter (fn [x] (.endsWith x ".xml"))
+  (filter (fn [x] (re-find #"/\d+.xml$" x))
           (for [file (file-seq (File. dir))] (str dir "/" (.getName file)))))
 
 (defn munge-namespace
@@ -17,11 +24,43 @@
 (defn retrieve-post-data
   [xml-data]
   {
-   :date ($x:text "//pubDate" xml-data)
+   :timestamp (date-str-to-timestamp ($x:text "//pubDate" xml-data))
    :title ($x:text "//title" xml-data)
    :id ($x:text "//wp_post_id" xml-data)
    })
 
+(defn date-str-to-timestamp
+  [date-str]
+  (def df (java.text.SimpleDateFormat. "EEE MMM d HH:mm:ss zzz yyyy"))
+  (.getTime (.parse df date-str)))
+
+(defn get-desired-timestamp
+  []
+  (-  (.getTime (Date.))
+      ms-diff
+      ( * 7 24 3600 1000)))
+
+
+(defn get-post
+  [filename]
+  (-> filename slurp munge-namespace retrieve-post-data)
+  )
+
 (defn get-post-data
   [dir]
-  (map (fn[x] (-> x slurp munge-namespace retrieve-post-data))  (get-post-filenames dir)))
+  (map get-post (get-post-filenames dir)))
+
+(defn get-closest-posts
+  [dir]
+  (take 5
+   (filter
+    (fn [x] (> (:timestamp x) (get-desired-timestamp)))
+    (sort-by :timestamp (get-post-data dir)))))
+
+(defn format-post
+  [post]
+  (def ymd (java.text.SimpleDateFormat. "yyyy MMM d"))
+  (format
+   "* [%s] %s"
+   (.format ymd (Date. (:timestamp post)))
+   (:title post)))
